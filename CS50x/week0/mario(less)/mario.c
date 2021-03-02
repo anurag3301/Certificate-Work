26a60fe8fde7238540ec3ee342218aab94cdc4e8
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-03-02 17:06:46 +0530

    Added mario(less)

---
+++ CS50x/week0/mario(less)/mario.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    while (true)
+    {
+        //Lets take the input
+        int input = get_int("Height: ");
+        if (input > 0 && input < 9)
+        {
+            for (int i = 1; i <= input; i++)
+            {
+                for (int j = 1; j <= input - i; j++)
+                {
+                    printf(" ");
+                }
+                
+                for (int j = 1; j <= i ; j++)
+                {
+                    printf("#");
+                }
+                printf("\n");
+            }
+            return 0;
+        }
+    }
+}
