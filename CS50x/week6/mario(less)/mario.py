057bf0da5b413dc15fd1b43cae80a6b6f87560d0
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-03-02 17:43:22 +0530

    Added python/mario(less)

---
+++ CS50x/week6/mario(less)/mario.py
@@ -0,0 +1,14 @@
+
+while True:
+    n = input("Height: ")
+    try:
+        n = int(n)
+        if n > 0 and n < 9:
+            break
+    except:
+        pass
+
+for i in range(n):
+    x = " " * (n - i - 1)
+    y = "#" * (i + 1)
+    print(x + y)
