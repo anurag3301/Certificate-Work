2e34d80437cbe313297ada4c051973c099b3b97d
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2022-01-31 12:31:22 +0530

    Added CS50x/week1/cash:2022

--- CS50x/week1/cash/cash.c
+++ CS50x/week1/cash/cash.c
@@ -1,32 +1,71 @@
-#include <stdio.h>
 #include <cs50.h>
-#include <math.h>
+#include <stdio.h>
+
+int get_cents(void);
+int calculate_quarters(int cents);
+int calculate_dimes(int cents);
+int calculate_nickels(int cents);
+int calculate_pennies(int cents);
 
 int main(void)
 {
-    while (true)
+    // Ask how many cents the customer is owed
+    int cents = get_cents();
+
+    // Calculate the number of quarters to give the customer
+    int quarters = calculate_quarters(cents);
+    cents = cents - quarters * 25;
+
+    // Calculate the number of dimes to give the customer
+    int dimes = calculate_dimes(cents);
+    cents = cents - dimes * 10;
+
+    // Calculate the number of nickels to give the customer
+    int nickels = calculate_nickels(cents);
+    cents = cents - nickels * 5;
+
+    // Calculate the number of pennies to give the customer
+    int pennies = calculate_pennies(cents);
+    cents = cents - pennies * 1;
+
+    // Sum coins
+    int coins = quarters + dimes + nickels + pennies;
+
+    // Print total number of coins to give the customer
+    printf("%i\n", coins);
+}
+
+int get_cents(void)
+{
+    int dollers;
+    do
     {
-        // Get user input
-        float dollars = get_float("Change owed: ");
-        if (dollars > 0.0)
-        {
-            /* do the magic*/
-            
-            int cents = round(dollars * 100);
-            int val25 = cents / 25;
-            cents -= val25 * 25;
-            
-            int val10 = cents / 10;
-            cents -= val10 * 10;
-            
-            int val5 = cents / 5;
-            cents -= val5 * 5;
-            
-            int val1 = cents / 1;
-            cents -= val1 * 1;
-            
-            printf("%i\n", val10 + val25 + val5 + val1);
-            return 0;
-        }
+        dollers = get_int("Enter dollers: ");
     }
+    while (dollers < 0);
+    return dollers;
+}
+
+int calculate_quarters(int cents)
+{
+    // TODO
+    return cents / 25;
+}
+
+int calculate_dimes(int cents)
+{
+    // TODO
+    return cents / 10;
+}
+
+int calculate_nickels(int cents)
+{
+    // TODO
+    return cents / 5;
+}
+
+int calculate_pennies(int cents)
+{
+    // TODO
+    return cents / 1;
 }
