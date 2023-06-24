f14b5002f424c7e513dab77e64aba1d6951c7272
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2023-06-25 00:08:36 +0530

    Added code for the hashmap dictinary for week5 speller

--- CS50x/week5/speller/dictionary.c
+++ CS50x/week5/speller/dictionary.c
@@ -2,8 +2,23 @@
 
 #include <ctype.h>
 #include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
 #include "dictionary.h"
+
+uint32_t count = 0u;
+
+void lower(char *str)
+{
+    for (size_t i = 0; str[i] != '\0'; i++)
+    {
+        str[i] = tolower(str[i]);
+    }
+}
 
 // Represents a node in a hash table
 typedef struct node
@@ -14,7 +29,7 @@ typedef struct node
 node;
 
 // TODO: Choose number of buckets in hash table
-const unsigned int N = 26;
+const unsigned int N = 10000;
 
 // Hash table
 node *table[N];
@@ -22,34 +37,112 @@ node *table[N];
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
-    // TODO
+    char copy[100];
+    strcpy(copy, word);
+    lower(copy);
+    uint32_t hash_val = hash(copy);
+    node *head = table[hash_val];
+    while (head != NULL)
+    {
+        if (strcmp(head->word, copy) == 0)
+        {
+            return true;
+        }
+        head = head->next;
+    }
     return false;
 }
 
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-    // TODO: Improve this hash function
-    return toupper(word[0]) - 'A';
+    unsigned int hash_val = 0;
+    for (size_t i = 0; word[i] != '\0'; i++)
+    {
+        hash_val += word[i];
+    }
+    return hash_val;
+}
+
+void insert(node **head, node *new_node)
+{
+    if (*head == NULL)
+    {
+        *head = new_node;
+        return;
+    }
+    node *temp = *head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = new_node;
+}
+
+void print_rec(node *n)
+{
+    if (n == NULL)
+    {
+        return;
+    }
+    printf("%s -> ", n->word);
+    print_rec(n->next);
+}
+
+void print_table()
+{
+    for (size_t i = 0; i < N; i++)
+    {
+        if (table[i] != NULL)
+        {
+            print_rec(table[i]);
+            printf("\n");
+        }
+    }
 }
 
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    // TODO
-    return false;
+    FILE *in = fopen(dictionary, "r");  
+    char line[100];
+    size_t line_max = 0;  
+    while (fgets(line, sizeof(line), in) != NULL)
+    {
+        line[strcspn(line, "\n")] = '\0';
+        lower(line);
+        uint32_t hash_val = hash(line);
+        node *new_node = malloc(sizeof(node));
+        strcpy(new_node->word, line);
+        new_node->next = NULL;
+        insert(&table[hash_val], new_node);
+        count++;
+    }
+
+    fclose(in);
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
-    return 0;
+    return count;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
-    // TODO
-    return false;
+    for (size_t i = 0; i < N; i++)
+    {
+        node *head = table[i];
+        node *temp;
+        while (head != NULL)
+        {
+            temp = head->next;
+            free(head);
+            head = temp;
+        }
+        table[i] = NULL;
+    }
+    return true;
 }
